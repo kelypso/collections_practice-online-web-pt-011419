b98664851b538efeab9de3c7ac162d9327ef616c
Kelsey Low <kklowlow@gmail.com>
2019-01-22 01:55:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,7 +19,9 @@ end
 
 def kesha_maker(array)
   new_array = []
-  array.each { |element| new_array << element[2] = "$" }
+  array.each do |element| 
+    new_array << element[2] = "$"
+  end
   new_array
 end
 
