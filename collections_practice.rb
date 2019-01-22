d659771a26feb28c62aab54ba9ebaf050394138b
Kelsey Low <kklowlow@gmail.com>
2019-01-22 01:47:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by { |x| x.length }
+end
+
+def swap_elements(array)
+  array.sort { |a, b| a = b && b = a }
+end
+
+def reverse_array(array)
+end
+
+def kesha_maker(array)
+end
+
+def find_a(array)
+end
+
+def sum_array(array)
+end
+
+def add_s(array)
+end
